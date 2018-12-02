efc894a32f60d10f3b8b3edfa9cd7531d70f4e81
Tomi Chen <tomichen33@gmail.com>
2018-12-01 17:31:15 -0800

    Add files

---
+++ script.js
@@ -0,0 +1,10 @@
+function send(){
+    let url = document.forms["form"].elements[0].value
+    let send = document.forms["form"].elements[1].value
+    $.post(url, send, function(data, status){
+	$("#results").text(data);
+        $("#status").text(status);
+    });
+
+}
+
